9a3ebee19dadd3bb847a34b46211e8174c778324
羿均 陳 <kitty.2006.25@gmail.com>
2026-05-03 21:54:53 +0800

    加入專案檔案。

---
+++ frmBeepPlayer.Designer.cs
@@ -0,0 +1,335 @@
+﻿namespace BeepPlayer
+{
+    partial class frmBeepPlayer
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.palMain = new System.Windows.Forms.Panel();
+            this.btn8 = new System.Windows.Forms.Button();
+            this.btn7 = new System.Windows.Forms.Button();
+            this.btn6 = new System.Windows.Forms.Button();
+            this.btn5 = new System.Windows.Forms.Button();
+            this.btn4 = new System.Windows.Forms.Button();
+            this.btn3 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.btnBlack1 = new System.Windows.Forms.Button();
+            this.btnBlack2 = new System.Windows.Forms.Button();
+            this.btnBlack3 = new System.Windows.Forms.Button();
+            this.btnBlack4 = new System.Windows.Forms.Button();
+            this.btnBlack5 = new System.Windows.Forms.Button();
+            this.palMain.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // palMain
+            // 
+            this.palMain.Controls.Add(this.btnBlack5);
+            this.palMain.Controls.Add(this.btnBlack4);
+            this.palMain.Controls.Add(this.btnBlack3);
+            this.palMain.Controls.Add(this.btnBlack2);
+            this.palMain.Controls.Add(this.btnBlack1);
+            this.palMain.Controls.Add(this.label8);
+            this.palMain.Controls.Add(this.label7);
+            this.palMain.Controls.Add(this.label6);
+            this.palMain.Controls.Add(this.label5);
+            this.palMain.Controls.Add(this.label4);
+            this.palMain.Controls.Add(this.label3);
+            this.palMain.Controls.Add(this.label2);
+            this.palMain.Controls.Add(this.label1);
+            this.palMain.Controls.Add(this.btn8);
+            this.palMain.Controls.Add(this.btn7);
+            this.palMain.Controls.Add(this.btn6);
+            this.palMain.Controls.Add(this.btn5);
+            this.palMain.Controls.Add(this.btn4);
+            this.palMain.Controls.Add(this.btn3);
+            this.palMain.Controls.Add(this.btn2);
+            this.palMain.Controls.Add(this.btn1);
+            this.palMain.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.palMain.Location = new System.Drawing.Point(0, 0);
+            this.palMain.Name = "palMain";
+            this.palMain.Size = new System.Drawing.Size(1249, 765);
+            this.palMain.TabIndex = 0;
+            // 
+            // btn8
+            // 
+            this.btn8.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btn8.ForeColor = System.Drawing.Color.Black;
+            this.btn8.Location = new System.Drawing.Point(1063, 36);
+            this.btn8.Name = "btn8";
+            this.btn8.Size = new System.Drawing.Size(141, 626);
+            this.btn8.TabIndex = 7;
+            this.btn8.UseVisualStyleBackColor = true;
+            // 
+            // btn7
+            // 
+            this.btn7.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btn7.Location = new System.Drawing.Point(916, 36);
+            this.btn7.Name = "btn7";
+            this.btn7.Size = new System.Drawing.Size(141, 626);
+            this.btn7.TabIndex = 6;
+            this.btn7.UseVisualStyleBackColor = true;
+            // 
+            // btn6
+            // 
+            this.btn6.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btn6.Location = new System.Drawing.Point(769, 36);
+            this.btn6.Name = "btn6";
+            this.btn6.Size = new System.Drawing.Size(141, 626);
+            this.btn6.TabIndex = 5;
+            this.btn6.UseVisualStyleBackColor = true;
+            // 
+            // btn5
+            // 
+            this.btn5.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btn5.Location = new System.Drawing.Point(622, 36);
+            this.btn5.Name = "btn5";
+            this.btn5.Size = new System.Drawing.Size(141, 626);
+            this.btn5.TabIndex = 4;
+            this.btn5.UseVisualStyleBackColor = true;
+            // 
+            // btn4
+            // 
+            this.btn4.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btn4.Location = new System.Drawing.Point(475, 36);
+            this.btn4.Name = "btn4";
+            this.btn4.Size = new System.Drawing.Size(141, 626);
+            this.btn4.TabIndex = 3;
+            this.btn4.UseVisualStyleBackColor = true;
+            // 
+            // btn3
+            // 
+            this.btn3.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btn3.Location = new System.Drawing.Point(328, 36);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(141, 626);
+            this.btn3.TabIndex = 2;
+            this.btn3.UseVisualStyleBackColor = true;
+            // 
+            // btn2
+            // 
+            this.btn2.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btn2.Location = new System.Drawing.Point(181, 36);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(141, 626);
+            this.btn2.TabIndex = 1;
+            this.btn2.UseVisualStyleBackColor = true;
+            // 
+            // btn1
+            // 
+            this.btn1.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btn1.Location = new System.Drawing.Point(34, 36);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(141, 626);
+            this.btn1.TabIndex = 0;
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("新細明體", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label1.Location = new System.Drawing.Point(70, 665);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 42);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Do";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("新細明體", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label2.Location = new System.Drawing.Point(219, 665);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 42);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Re";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("新細明體", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label3.Location = new System.Drawing.Point(365, 665);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(66, 42);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Mi";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("新細明體", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label4.Location = new System.Drawing.Point(515, 665);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(60, 42);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Fa";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("新細明體", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label5.Location = new System.Drawing.Point(655, 665);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(74, 42);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Sol";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("新細明體", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label6.Location = new System.Drawing.Point(808, 665);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(62, 42);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "La";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("新細明體", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label7.Location = new System.Drawing.Point(960, 665);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(53, 42);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "Si";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("新細明體", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label8.Location = new System.Drawing.Point(1099, 665);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(68, 42);
+            this.label8.TabIndex = 15;
+            this.label8.Text = "Do";
+            // 
+            // btnBlack1
+            // 
+            this.btnBlack1.BackColor = System.Drawing.SystemColors.WindowText;
+            this.btnBlack1.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btnBlack1.Location = new System.Drawing.Point(143, 36);
+            this.btnBlack1.Name = "btnBlack1";
+            this.btnBlack1.Size = new System.Drawing.Size(70, 375);
+            this.btnBlack1.TabIndex = 10;
+            this.btnBlack1.UseVisualStyleBackColor = false;
+            // 
+            // btnBlack2
+            // 
+            this.btnBlack2.BackColor = System.Drawing.SystemColors.WindowText;
+            this.btnBlack2.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btnBlack2.Location = new System.Drawing.Point(291, 36);
+            this.btnBlack2.Name = "btnBlack2";
+            this.btnBlack2.Size = new System.Drawing.Size(70, 375);
+            this.btnBlack2.TabIndex = 11;
+            this.btnBlack2.UseVisualStyleBackColor = false;
+            // 
+            // btnBlack3
+            // 
+            this.btnBlack3.BackColor = System.Drawing.SystemColors.WindowText;
+            this.btnBlack3.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btnBlack3.Location = new System.Drawing.Point(584, 36);
+            this.btnBlack3.Name = "btnBlack3";
+            this.btnBlack3.Size = new System.Drawing.Size(70, 375);
+            this.btnBlack3.TabIndex = 12;
+            this.btnBlack3.UseVisualStyleBackColor = false;
+            // 
+            // btnBlack4
+            // 
+            this.btnBlack4.BackColor = System.Drawing.SystemColors.WindowText;
+            this.btnBlack4.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btnBlack4.Location = new System.Drawing.Point(731, 36);
+            this.btnBlack4.Name = "btnBlack4";
+            this.btnBlack4.Size = new System.Drawing.Size(70, 375);
+            this.btnBlack4.TabIndex = 13;
+            this.btnBlack4.UseVisualStyleBackColor = false;
+            // 
+            // btnBlack5
+            // 
+            this.btnBlack5.BackColor = System.Drawing.SystemColors.WindowText;
+            this.btnBlack5.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btnBlack5.Location = new System.Drawing.Point(879, 36);
+            this.btnBlack5.Name = "btnBlack5";
+            this.btnBlack5.Size = new System.Drawing.Size(70, 375);
+            this.btnBlack5.TabIndex = 14;
+            this.btnBlack5.UseVisualStyleBackColor = false;
+            // 
+            // frmBeepPlayer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 27F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.ClientSize = new System.Drawing.Size(1249, 765);
+            this.Controls.Add(this.palMain);
+            this.Name = "frmBeepPlayer";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "簡易電子琴";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.frmBeepPlayer_FormClosing);
+            this.Load += new System.EventHandler(this.frmBeepPlayer_Load);
+            this.SizeChanged += new System.EventHandler(this.frmBeepPlayer_SizeChanged);
+            this.palMain.ResumeLayout(false);
+            this.palMain.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel palMain;
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Button btn8;
+        private System.Windows.Forms.Button btn7;
+        private System.Windows.Forms.Button btn6;
+        private System.Windows.Forms.Button btn5;
+        private System.Windows.Forms.Button btn4;
+        private System.Windows.Forms.Button btn3;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnBlack1;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnBlack5;
+        private System.Windows.Forms.Button btnBlack4;
+        private System.Windows.Forms.Button btnBlack3;
+        private System.Windows.Forms.Button btnBlack2;
+    }
+}
+
---
+++ frmBeepPlayer.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Forms;
+
+namespace BeepPlayer
+{
+    public partial class frmBeepPlayer : Form
+    {
+
+        [DllImport("kernel32.dll")]
+        public static extern bool Beep(int frequency, int duration);
+        int[] freq = { 523, 587, 659, 698, 784, 880, 988, 1046 };
+        int[] blackFreq = { 554, 622, 740, 831, 932 };
+        int initWidth= 0;
+        int initHeight = 0;
+        Dictionary<string, Rect> initControl = new Dictionary<string, Rect>();
+        public frmBeepPlayer()
+        {
+            InitializeComponent();
+            this.DoubleBuffered = true;
+
+            foreach (Control ctl in this.palMain.Controls)
+            {
+                if (ctl is Button btn && btn.Name.StartsWith("btn"))
+                {
+                    // 先減掉可能的重複綁定，再加回來，保證只會發聲一次
+                    btn.Click -= btn1_Click;
+                    btn.Click += btn1_Click;
+                }
+            }
+            InitializePianoStyle(); // 套用鋼琴外觀
+        }
+
+        private void InitializePianoStyle()
+        {
+            foreach (Control ctl in this.palMain.Controls)
+            {
+
+                if (ctl is Button btn)
+                {
+                    // 根據名稱決定初始顏色
+                    if (btn.Name.Contains("Black"))
+                    {
+                        btn.BackColor = Color.Black;
+                        btn.ForeColor = Color.White;
+                    }
+                    else
+                    {
+                        btn.BackColor = Color.White;
+                        btn.ForeColor = Color.Black;
+                    }
+
+                    btn.FlatStyle = FlatStyle.Flat;
+                    btn.FlatAppearance.BorderColor = Color.LightGray;
+                    btn.Cursor = Cursors.Hand;
+                    btn.FlatAppearance.BorderSize = 0;
+                }
+            }
+        }
+
+        private async void btn1_Click(object sender, EventArgs e)
+        {
+            if (sender is Button btn)
+            {
+                // 1. 儲存原始顏色，以便發聲結束後還原
+                Color oldColor = btn.BackColor;
+
+                // 2. 視覺回饋：根據黑白鍵給予不同的「按下」效果
+                // 如果名稱包含 "Black"，變更為深灰色 (DimGray)；否則變更為淺灰色 (Gainsboro)
+                btn.BackColor = btn.Name.Contains("Black") ? Color.DimGray : Color.Gainsboro;
+
+                // 3. 停用按鈕，防止使用者在播放期間連續點擊導致音訊堆疊或程式當掉
+                btn.Enabled = false;
+
+                try
+                {
+                    int frequency;
+
+                    // 4. 根據按鈕名稱動態決定讀取哪一個頻率陣列
+                    if (btn.Name.Contains("Black"))
+                    {
+                        // 因為黑鍵 TabIndex 是 10-14，所以要減掉 10 才能對應到 blackFreq 的 0-4
+                        int index = btn.TabIndex - 10;
+
+                        // 確保 index 在合法範圍內 (0 到 4)
+                        index = Math.Max(0, Math.Min(index, blackFreq.Length - 1));
+                        frequency = blackFreq[index];
+                    }
+                    else
+                    {
+                        // 白鍵 TabIndex 是 0-7，直接使用即可
+                        int index = Math.Max(0, Math.Min(btn.TabIndex, freq.Length - 1));
+                        frequency = freq[index];
+                    }
+
+                    // 5. 使用非同步 Task.Run 呼叫底層 API，避免 Beep 期間 UI 視窗凍結 (Not Responding)
+                    // 持續時間設為 300 毫秒
+                    await Task.Run(() => Beep(frequency, 300));
+                }
+                catch (Exception ex)
+                {
+                    // 捕捉可能的系統異常並顯示，增強程式健壯性
+                    MessageBox.Show($"播放音效時發生錯誤：{ex.Message}", "系統錯誤", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                finally
+                {
+                    // 6. 無論播放成功與否，最後都要恢復按鈕狀態
+                    btn.Enabled = true;
+                    btn.BackColor = oldColor;
+                }
+            }
+        }
+
+        private void frmBeepPlayer_Load(object sender, EventArgs e)
+        {
+            //儲存初始視窗大小
+            this.initWidth = this.palMain.Width;
+            this.initHeight = this.palMain.Height;
+            //儲存初始控制像位置和大小
+            foreach (Control ctl in this.palMain.Controls)
+            {
+                this.initControl.Add(ctl.Name, new Rect(ctl.Left, ctl.Top, ctl.Width, ctl.Height));
+            }
+        }
+
+        /// <summary>
+        /// 當視窗改變大小 根據初始大小和控制項位置調整控制項的位置和大小
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void frmBeepPlayer_SizeChanged(object sender, EventArgs e)
+        {
+            this.SuspendLayout();
+
+            double width = this.palMain.Width;
+            double height = this.palMain.Height;
+            double iRatioWith = width / this.initWidth;
+            double iRatioHeight = height / this.initHeight;
+            foreach (Control ctl in this.palMain.Controls)
+            {
+                if (initControl.ContainsKey(ctl.Name))
+                {
+                    ctl.Left = (int)(initControl[ctl.Name].Left * iRatioWith);
+                    ctl.Top = (int)(initControl[ctl.Name].Top * iRatioHeight);
+                    ctl.Width = (int)(initControl[ctl.Name].Width * iRatioWith);
+                    ctl.Height = (int)(initControl[ctl.Name].Height * iRatioHeight);
+                }
+            }
+            this.ResumeLayout();
+        }
+
+        private void frmBeepPlayer_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            var result = MessageBox.Show("確定要關閉應用程式嗎？", "關閉確認", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (result == DialogResult.No)
+            {
+                e.Cancel = true; // 取消關閉
+            }
+        }
+    }
+}
